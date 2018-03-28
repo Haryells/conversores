b4e4ba71f548113e7f17398a64162ba1535eca12
Haryell <haryellsd@gmail.com>
2018-03-27 21:35:56 -0300

    Inicio do desenvolvimento do app.

---
+++ Conversores/Conversores/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Conversores
+//
+//  Created by Haryell  on 27/03/2018.
+//  Copyright © 2018 Haryell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btUnitOne: UIButton!
+    @IBOutlet weak var btUnitTwo: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showNext(_ sender: UIButton) {
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+    }
+    
+    
+}
+
